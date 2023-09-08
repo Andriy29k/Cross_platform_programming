fc72af1461233bd94ef4b52504dce67eccf8ba05
Andrii <105487432+Andriy29k@users.noreply.github.com>
2023-09-08 18:01:57 +0300

    Add created exeptions

---
+++ Lab4/DiskFullException.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class DiskFullException extends Exception {
+        public DiskFullException(String message) {
+            super(message);
+        }
+}
---
+++ Lab4/ScannerExeption.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class ScannerExeption extends Exception{
+    public ScannerExeption(String message){
+        super(message);
+    }
+}
+
